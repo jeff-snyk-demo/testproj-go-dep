dbc111fca3b753575eae79d145b0c4ff4acd4369
Jeff <jeff@snyk.io>
2019-05-19 12:33:59 -0400

    initial commit

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	accounting "github.com/leekchan/accounting"
+	"github.com/aws/aws-sdk-go"
+	"cloud.google.com/go/storage"
+)
+
+func main() {
+	fmt.Println("hello again")
+
+	// ac := accounting.Accounting{Symbol: "$", Precision: 2}
+	// fmt.Println(ac.FormatMoney(123456789.213123))
+}
